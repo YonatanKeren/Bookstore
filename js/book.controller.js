8d23147bbd00758e058d4e0b38e1f0c538a2d721
Yonatan Keren <yonatan.keren1@gmail.com>
2025-03-22 15:19:34 +0200

    Apply render() and remove hard coded table

--- js/book.controller.js
+++ js/book.controller.js
@@ -1,9 +1,37 @@
 'use strict'
 
-function onInit(){
-    render()
+function onInit() {
+    const books = getBooks()
+    console.log(books)
+    render(books)
 }
 
-function render(){
-    console.log('Rendering...')
+function render(books) {
+    var strHTML = ''
+    strHTML +=
+        `<thead>
+    <tr>
+        <th>Title</th>
+        <th>Price</th>
+        <th>Actions</th>
+    </tr>
+    </thead>
+    <tbody>
+    `;
+    books.forEach(item => strHTML += renderRow(item))
+    strHTML += `</tbody>`;
+    document.querySelector('.book-table').innerHTML += strHTML
+}
+
+function renderRow(book) {
+    return `
+    <tr>
+                <td>${book.title}</td>
+                <td>${book.price}</td>
+                <td><button>Read</button>
+                    <button>Update</button>
+                    <button>Delete</button>
+                </td>
+            </tr >
+            `;
 }
--- js/book.service.js
+++ js/book.service.js
@@ -1,11 +1,33 @@
 'use strict'
 
+
+
+//add random id function?
+const book = {
+    id: "aa0000",
+    title: 'Book',
+    price: 120,
+    // imgUrl: 'book.jpg'
+}
+const booker = {
+    id: "aa0001",
+    title: 'Booker',
+    price: 300,
+    // imgUrl: 'booker.jpg'
+}
+const bookest = {
+    id: "aa0002",
+    title: 'Bookest',
+    price: 999,
+    // imgUrl: 'bookest.jpg'
+}
+
 const gBooks = [
-    'Book',
-    'Booker',
-    'Bookest'
+    book,
+    booker,
+    bookest
 ]
 
-function getBooks(){
+function getBooks() {
     return gBooks
 }
