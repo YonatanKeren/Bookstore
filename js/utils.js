e3fa06be51b6b1b82caacf469347f83f18219f67
Yonatan Keren <yonatan.keren1@gmail.com>
2025-03-22 17:50:01 +0200

    Add utils.js file

---
+++ js/utils.js
@@ -0,0 +1,28 @@
+'use strict'
+
+// get Random integer variations
+
+function getRndIntExcMax(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function getRndIntIncMax(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+//colors
+
+function getRandomColor() {
+  const letters = '0123456789ABCDEF'
+  var color = '#'
+
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)]
+  }
+  return color
+}
+
+function getRandomLetter() {
+  const letters = 'abcdefghijklmnopqrstuvwxyz'
+  return letters[Math.floor(Math.random() * 26)]
+}
